fa09b5d54c33fb7bb7706e1e3b49add74b191fd7
18WH1A1209 <69299906+18WH1A1209@users.noreply.github.com>
2021-07-07 22:43:25 +0530

    Add files via upload

---
+++ SL Lab/Experiment 3/Exp3b.rb
@@ -0,0 +1,13 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+#Method1
+puts "Enter First Name"
+str1 = gets.chomp
+puts "Enter Last Name"
+str2 = gets.chomp
+puts "The result is #{str2} #{str1}"
