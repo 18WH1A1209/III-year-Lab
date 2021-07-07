7fb3cd537252829fa4b8afdb37cedb3db675b4ff
18WH1A1209 <69299906+18WH1A1209@users.noreply.github.com>
2021-07-07 23:01:57 +0530

    Add files via upload

---
+++ SL Lab/Experiment 9/Exp9.rb
@@ -0,0 +1,12 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+
+array1 = ["Ruby", 2.3, Time.now,1,8]
+for array_element in array1
+          puts array_element
+end
