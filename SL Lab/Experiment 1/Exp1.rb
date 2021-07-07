4aa0940170b75a93e1dbfa23999bfe7b25c585b6
18WH1A1209 <69299906+18WH1A1209@users.noreply.github.com>
2021-07-07 22:28:03 +0530

    Add files via upload

---
+++ SL Lab/Experiment 1/Exp1.rb
@@ -0,0 +1,13 @@
+=begin
+
+                            Online Ruby Compiler.
+                Code, Compile, Run and Debug Ruby script online.
+Write your code in this editor and press "Run" button to execute it.
+
+=end
+
+def stringcopy(str,n)
+    return str*n
+end
+puts "The output after string copy is: "
+puts stringcopy('Hello',2)
